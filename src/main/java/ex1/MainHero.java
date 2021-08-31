6679871d9a8fed61e98367c7c7d8f0bfb8e3b937
szymonstankowski <szymonstankowski1@gmail.com>
2021-08-31 22:42:28 +0200

    Cwiczenia Stream - poprawione

--- src/main/java/ex1/MainHero.java
+++ src/main/java/ex1/MainHero.java
@@ -1,8 +1,5 @@
 package ex1;
 
-import org.springframework.cglib.core.Local;
-import org.springframework.format.datetime.joda.LocalDateParser;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,22 +8,47 @@ import java.util.stream.Collectors;
 public class MainHero {
     public static void main(String[] args) {
 
-        LocalDate localDate = LocalDate.now();
+//        Utwórz klasę MainHero
+//        W metodzie main utwórz listę obiektów typu Hero, a następnie przy pomocy strumieni:
+//
+//        - wybierz tylko tych bohaterów, których druga litera nazwy to u oraz ich moc jest większa od 5
+//        - sortuj bohaterów pod względem mocy (domyślnie zostanie wykorzystana utworzona metoda z interfejsu)
+//        - przekształć obiekty pobierając tylko ich nazwy
+//        - przekształć nazwy, zostawiając z nich tylko pierwszą literę oraz zamieniaj litery na wielkie,
+//        - zwróć przekształcone nazwy, połączone za pomocą znaku myślnika - w postaci jednego obiektu typu String
+//
+//        Wszystkie operacje wykonaj na jednym strumieniu. Przykładowy wynik:
 
-        List<Hero> list = new ArrayList<>();
-
-        list.add(new Hero("Superman", 100L, localDate, "123456"));
-        list.add(new Hero("Ironman", 85L, localDate, "0987"));
-        list.add(new Hero("Hulk", 90L, localDate, "1234567"));
 
 
-        list.stream()
-                .filter(hero -> hero.getName().matches(".[u][a-z]*") && hero.getPower() > 5L)
+        List<Hero> heroList = new ArrayList<>();
+
+
+        heroList.add(new Hero("Superman", 100L, LocalDate.of(2021,8,31), "123456"));
+        heroList.add(new Hero("Ironman", 85L, LocalDate.of(2021,8,31), "0987"));
+        heroList.add(new Hero("Hulk", 90L, LocalDate.of(2021,8,31), "1234567"));
+        heroList.add(new Hero("spiderman", 6L, LocalDate.of(2001, 12,11), "1234567890"));
+        heroList.add(new Hero("hubertman", 10l, LocalDate.of(2001, 12,11),"0987654321"));
+
+
+
+
+        String collect = heroList.stream()
+                //.filter(h -> h.getName().charAt(1) == 'u')
+                .filter(hero -> hero.getName().matches(".[u][a-z]*"))
+                .filter(hero -> hero.getPower()>5)
                 .sorted()
-                .forEach(hero -> System.out.println(hero.getName()+" "+hero.getPower()));
+                //.map(h -> h.getName().substring(o, 1).toUpperCase())
+                .map(hero -> hero.getName())
+                .map(n -> n.substring(0, 1))
+                .map(n -> n.toUpperCase())
+                .collect(Collectors.joining("-"));
+        System.out.println(collect);
 
-//        list.stream()
+//        heroList.stream()
 //                .forEach(hero -> System.out.println(hero.getName()));
+
+
 
 
     }
